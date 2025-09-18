539737218c86a08876e490aee6acbb6c7f004ad2
Martine R Karlsen <martinekar08@gmail.com>
2025-09-18 21:41:38 +0200

    farger, root, hover

--- invitation.js
+++ invitation.js
@@ -2,12 +2,9 @@ const openEnvelopeButton = document.getElementById("open-envelope");
 const envelope = document.querySelector(".envelope");
 
 openEnvelopeButton.addEventListener("click", () => {
-  envelope.classList.toggle("open"); // Toggle open/close
-
-  // Change button text based on envelope state
-  if (envelope.classList.contains("open")) {
-    openEnvelopeButton.textContent = "Lukk konvolutt";
-  } else {
-    openEnvelopeButton.textContent = "Åpne konvolutt";
-  }
+  envelope.classList.toggle("open");
+  openEnvelopeButton.classList.toggle("active"); // toggle button state
+  openEnvelopeButton.textContent = envelope.classList.contains("open")
+    ? "Lukk konvolutt"
+    : "Åpne konvolutt";
 });
